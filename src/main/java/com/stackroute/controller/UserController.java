95e36712b2434025c8b2c298daa65f4753affbfa
keerthika <atmakurikeerthika@gmail.com>
2019-08-02 11:10:34 +0530

    adding exception handling

--- src/main/java/com/stackroute/controller/UserController.java
+++ src/main/java/com/stackroute/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.stackroute.controller;
 
 import com.stackroute.domain.User;
+import com.stackroute.exceptions.UserAlreadyExistsException;
 import com.stackroute.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,18 +21,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("user")
+    @PostMapping("/user")
     public ResponseEntity<?> saveUser(@RequestBody User user) {
+      ResponseEntity responseEntity;
+      try {
         User savedUser = userService.saveUser(user);
-        return new ResponseEntity<>(savedUser, HttpStatus.OK);
-    }
+        responseEntity = new ResponseEntity<String>("succesfullycreated", HttpStatus.CREATED);
+      }
+      catch(UserAlreadyExistsException ex) {
+
+        responseEntity = new ResponseEntity<String>(ex.getMessage(),HttpStatus.CONFLICT);
+        ex.printStackTrace();
+      }
+      return responseEntity;
+      }
 
     @GetMapping("user/{id}")
     public ResponseEntity<?> getUserById(@PathVariable int id) {
         System.out.println(id);
         User retrivedUser = userService.getUserById(id);
         return new ResponseEntity<User>(retrivedUser, HttpStatus.OK);
-
-
     }
 }
